f8c9e47bff2aa52afd4b42777f01e078c49d3833
yazgoo <yazgoo@gmail.com>
2019-02-20 07:08:19 +0100

    add multiple layers

--- looperi.sh
+++ looperi.sh
@@ -2,8 +2,11 @@
 
 input() {
   stty raw
-  dd bs=1 count=1 2> /dev/null
+  key=$(dd bs=1 count=1 2> /dev/null)
+  rc=$?
+  [ $rc -ne 0 ] && return $rc
   stty -raw
+  return 0
 }
 
 record_background() {
@@ -15,43 +18,69 @@ record_background() {
   kill $pid
 }
 
-play_layer() {
-  play_layer_pid=""
-  if [ -e "layer.wav" ]
-  then
-    aplay layer.wav &
-    play_layer_pid=$!
-  fi
+play_layers() {
+  play_layer_pids=""
+  for i in $(seq $layer_number)
+  do
+    aplay layer$i.wav &
+    play_layer_pids="$! $play_layer_pids"
+  done
 }
 
 record_layer() {
   record_layer_pid=""
   if $(cat should_record_layer)
   then
-    arecord -f S16_LE -r 48000 -D hw:1,0 layer.wav &
+    layer_number=$[ $layer_number + 1 ]
+    arecord -f S16_LE -r 48000 -D hw:1,0 layer${layer_number}.wav &
     record_layer_pid=$!
     echo false > should_record_layer
   fi
 }
 
+remove_layer() {
+  record_layer_pid=""
+  if $(cat should_remove_layer)
+  then
+    layer_number=$[ $layer_number - 1 ]
+    echo false > should_remove_layer
+  fi
+}
+
 main_loop() {
   echo false > should_record_layer
+  echo false > should_remove_layer
+  layer_number=0
   while true
   do
-    play_layer
-    record_layer  
+    remove_layer
+    play_layers
+    record_layer
     aplay background.wav
     [ $? -ne 0 ] && break
-    [ -n $record_layer_pid ] && kill $record_layer_pid
-    [ -n $play_layer_pid ] && kill $play_layer_pid
+    [ -n "$record_layer_pid" ] && kill $record_layer_pid
+    [ -n "$play_layer_pids" ] && kill $play_layer_pids
   done
 }
 
-rm background.wav layer.wav
+input_loop() {
+  while true
+  do
+    input
+    [ "$key" = $'\003' ] && break
+    if [ "$key" = "a" ]
+    then
+      echo true > should_remove_layer
+    else
+      echo true > should_record_layer
+    fi
+    sleep 1
+  done
+}
+
+rm background.wav layer*.wav
 record_background
 main_loop &
 main_loop_pid=$!
-input
-echo true > should_record_layer
+input_loop
 wait $main_loop_pid
-
